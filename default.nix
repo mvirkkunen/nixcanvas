60f7b8e6318cbdabcc86b89fde0be046615eeed0
Matti Virkkunen <mvirkkunen@gmail.com>
2020-04-29 23:29:37 +0300

    Yuck

--- default.nix
+++ default.nix
@@ -3,4 +3,13 @@ with import <nixpkgs> {};
 mkYarnPackage {
   name = "nixcanvas";
   src = ./.;
+  pkgConfig.canvas = {
+    buildInputs = [ python3 ];
+    postInstall = ''
+      if [ -f ../.bin/node-pre-gyp ]; then
+        patchShebangs ../node-pre-gyp/
+        ../.bin/node-pre-gyp install --fallback-to-build
+      fi
+    '';
+  };
 }
