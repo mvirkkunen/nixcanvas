2ca768361d133a1f4698fff98e0fbc19d19aa897
Matti Virkkunen <mvirkkunen@gmail.com>
2020-04-30 00:15:22 +0300

    Yuck 2.0

--- default.nix
+++ default.nix
@@ -4,11 +4,11 @@ mkYarnPackage {
   name = "nixcanvas";
   src = ./.;
   pkgConfig.canvas = {
-    buildInputs = [ python3 ];
+    buildInputs = [ cairo freetype libjpeg libpng pango pkg-config pixman python3 ];
     postInstall = ''
       if [ -f ../.bin/node-pre-gyp ]; then
         patchShebangs ../node-pre-gyp/
-        ../.bin/node-pre-gyp install --fallback-to-build
+        ../.bin/node-pre-gyp install --build-from-source --nodedir=${nodejs}
       fi
     '';
   };
