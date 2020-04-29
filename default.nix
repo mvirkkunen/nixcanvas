d0d61a78db720ad60f36218e6dacc07fcedda505
Matti Virkkunen <mvirkkunen@gmail.com>
2020-04-30 00:43:02 +0300

    Slightly more palatable

--- default.nix
+++ default.nix
@@ -6,9 +6,8 @@ mkYarnPackage {
   pkgConfig.canvas = {
     buildInputs = [ cairo freetype libjpeg libpng pango pkg-config pixman python3 ];
     postInstall = ''
-      if [ -f ../.bin/node-pre-gyp ]; then
-        patchShebangs ../node-pre-gyp/
-        ../.bin/node-pre-gyp install --build-from-source --nodedir=${nodejs}
+      if [[ "$PWD" != /build/yarn_home/.cache/* ]]; then
+        node ../.bin/node-pre-gyp install --build-from-source --nodedir=${nodejs}
       fi
     '';
   };
